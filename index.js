8cda521bb1e9d9d39ee62025daf9b00253947ec2
marketaDevHenry <dev.marketadigital@gmail.com>
2023-10-04 19:57:45 -0300

    Setup files

---
+++ index.js
@@ -0,0 +1,16 @@
+function SvelteComponents () {
+   return {
+      components: [
+         {
+            name: 'Component 01',
+            html: '<Component-01 />'
+         },
+         {
+            name: 'Component 02',
+            html: '<Component-02 />'
+         }
+      ]
+   }
+}
+
+module.exports = SvelteComponents;
